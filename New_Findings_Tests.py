f5fa32cf12c17c5e79a16aa3d59063a2a8f0253a
SeriousCoffeeCup <seriouscoffeecup@gmail.com>
2024-10-08 01:32:05 -0700

    Refine profiling code methods e.g. timeit()

--- New_Findings_Tests.py
+++ New_Findings_Tests.py
@@ -8,14 +8,12 @@ import timeit
 # The Decorator Function, activated by placing @function_name over another function.
 def profile_this_function(func):
     def wrapper(*arguments, **keyworded_arguments):
-        option = 0
+        option = 1
         if option == 0:
             profiler = cProfile.Profile(timeunit=0.000001)  # Time units default to 1.0 seconds.
-
             profiler.enable()
+
             result = func(*arguments, **keyworded_arguments)
-
-
 
             profiler.disable()
 
@@ -25,19 +23,24 @@ def profile_this_function(func):
             return result
 
         elif option == 1:
-            print("Start time recorded.")
             start_time = timeit.default_timer()
 
             result = func(*arguments, **keyworded_arguments)
 
             time_difference = timeit.default_timer() - start_time
-            print("Function took", time_difference, "seconds.")
+
+            print(f"Function took {time_difference} seconds.")
 
             return result
 
     return wrapper
 
 @profile_this_function
-def test_massprimeduction(): # profiler needs to be called at moment of function creation. Trying this out
+def test_massprimeduction():
+    """
+    Decorator must precede a newly created function.
+    """
     New_Findings_Annotated.massprimeduction(200)
-test_massprimeduction()
+
+if __name__ == "__main__":
+    test_massprimeduction()
