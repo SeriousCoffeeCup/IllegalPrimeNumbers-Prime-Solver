eff2fb8a99b399db9e951018fa143b6756e60b29
SeriousCoffeeCup <seriouscoffeecup@gmail.com>
2024-10-07 21:33:02 -0700

    Runtime profiling code added to Tests.py
    Need greater precision next

--- New_Findings_Tests.py
+++ New_Findings_Tests.py
@@ -1,3 +1,43 @@
-from New_Findings import *
+import New_Findings
+import New_Findings_Annotated
 
-massprimeduction(200)
+import cProfile
+import pstats
+import timeit
+
+# The Decorator Function, activated by placing @function_name over another function.
+def profile_this_function(func):
+    def wrapper(*arguments, **keyworded_arguments):
+        option = 0
+        if option == 0:
+            profiler = cProfile.Profile(timeunit=0.000001)  # Time units default to 1.0 seconds.
+
+            profiler.enable()
+            result = func(*arguments, **keyworded_arguments)
+
+
+
+            profiler.disable()
+
+            stats_o_profiler = pstats.Stats(profiler).strip_dirs().sort_stats(
+                'cumulative')  # See other sort-bys available https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
+            stats_o_profiler.print_stats()
+            return result
+
+        elif option == 1:
+            print("Start time recorded.")
+            start_time = timeit.default_timer()
+
+            result = func(*arguments, **keyworded_arguments)
+
+            time_difference = timeit.default_timer() - start_time
+            print("Function took", time_difference, "seconds.")
+
+            return result
+
+    return wrapper
+
+@profile_this_function
+def test_massprimeduction(): # profiler needs to be called at moment of function creation. Trying this out
+    New_Findings_Annotated.massprimeduction(200)
+test_massprimeduction()
